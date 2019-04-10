b9924074272c1ecbc350429fb861f5a469fc91d7
HenrySylow <48073144+HenrySylow@users.noreply.github.com>
2019-04-10 22:46:17 +1000

    Update main.c

--- main.c
+++ main.c
@@ -8,9 +8,14 @@ Menu System
 */
 int main(){
     printf("Welcome to Henry's Encryption and Decryption Services\n");
-    printf("Press enter if you want to continue");
-    scanf("%d");
-    printf("are you sure\n");l
+    printf("If you want to encrypt a message with a rotaion cipher press 1\n");
+    printf("if you want to decrypt a message encyrpted with a rotaion cipher and you have the rotaion key press 2\n");
+    printf("If you want to encrypt a message using a substitution cipher press 3\n");
+    printf("If you want to decrypt a message with a substitution cipher with the known substitutions press 4\n");
+    printf("If you want to decrypt a message encrypted with a rotaion cipher without the \"cipher hey\" press 5\n");
+    printf("If you want to decrypt a message with a message encrypted with a substitution cipher without the \"cipher key\" press 6\n");
+
+
     return 0;
 
 }
