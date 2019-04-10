82ea0ba254f046d68b469366bd7552256f077e88
HenrySylow <48073144+HenrySylow@users.noreply.github.com>
2019-04-10 22:27:29 +1000

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+/* TO DO LIST
+Menu System
+
+
+
+*/
+int main(){
+    printf("Welcome to Henry's Encryption and Decryption Services\n");
+    printf("Press enter if you want to continue");
+    scanf("%d");
+    printf("are you sure\n");l
+    return 0;
+
+}
